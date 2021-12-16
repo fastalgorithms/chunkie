30653ef7b384476a20933f0564b64aed2e076403
Manas Rachh <mrachh@flatrioninstitute.org>
2021-12-16 15:18:57 -0500

    updating plot and adding normals as a property

--- chunkie/@chunker/merge.m
+++ chunkie/@chunker/merge.m
@@ -1,29 +1,60 @@
 function chnkrout = merge(chnkrs)
 
 if isempty(chnkrs)
-    chnkrout = chunker();
-    return
+  chnkrout = chunker();
+  return
 end
 assert(isa(chnkrs,'chunker'), 'input must be of chunker type');
 
-chnkrout = chnkrs(1);
+chnkrout = chunker();
+%chnkrout = chnkrs(1);
 
-for i = 2:length(chnkrs)
-    chnkrtemp = chnkrs(i);
-    assert(chnkrtemp.dim == chnkrout.dim,...
-        'chunkers to merge must be in same dimension');
-    nch = chnkrtemp.nch;
-    nchold = chnkrout.nch;
-    istart = nchold+1;
-    iend = istart+nch-1;
-    
-    chnkrout = chnkrout.addchunk(nch);
-    chnkrout.r(:,:,istart:iend) = chnkrtemp.r;
-    chnkrout.d(:,:,istart:iend) = chnkrtemp.d;
-    chnkrout.d2(:,:,istart:iend) = chnkrtemp.d2;
-    chnkrout.adj(:,istart:iend) = chnkrtemp.adj + nchold;
-    chnkrout.h(istart:iend) = chnkrtemp.h;
-    
+
+for i = 1:length(chnkrs)
+  chnkrtemp = chnkrs(i);
+  assert(chnkrtemp.dim == chnkrout.dim,...
+      'chunkers to merge must be in same dimension');
+  nch = chnkrtemp.nch;
+  nchold = chnkrout.nch;
+  istart = nchold+1;
+  iend = istart+nch-1;
+
+  chnkrout = chnkrout.addchunk(nch);
+  chnkrout.r(:,:,istart:iend) = chnkrtemp.r;
+  chnkrout.d(:,:,istart:iend) = chnkrtemp.d;
+  chnkrout.d2(:,:,istart:iend) = chnkrtemp.d2;
+  chnkrout.adj(:,istart:iend) = chnkrtemp.adj + nchold;
+  chnkrout.h(istart:iend) = chnkrtemp.h;
+  chnkrout.n(:,:,istart:iend) = chnkrtemp.n;
 end
+
+
+mmax = 0;
+nchold = 0;
+for i=1:length(chnkrs)
+    if(chnkrs(i).hasdata)
+        [m,~,~] = size(chnkrs(i).data);
+        if(m>mmax) 
+            mmax = m;
+        end
+    end
+end
+if(mmax>0)
+    chnkrout = chnkrout.makedatarows(mmax);
+    for i=1:length(chnkrs)
+      chnkrtemp = chnkrs(i);
+  
+      nch = chnkrtemp.nch;
+      istart = nchold+1;
+      iend = istart+nch-1;
+      
+      if(chnkrtemp.hasdata)
+        [m,~,~] = size(chnkrtemp.data);
+        chnkrout.data(1:m,:,istart:iend) = chnkrtemp.data;
+      end
+      nchold = nchold + nch;
+    end
+end
+
 
     
