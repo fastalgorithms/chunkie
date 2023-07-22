e5054bdbea5bffb425b4ce3dbcab6a3382cdc801
Manas Rachh <mrachh@flatironinstitute.org>
2023-07-21 20:28:00 -0400

    updating simple_clm_test

--- chunkie/clmtest/simple_clm_test.m
+++ chunkie/clmtest/simple_clm_test.m
@@ -9,9 +9,9 @@ geom_class.ndomain = ndomain;
 ncurve = 1;
 geom_class.ncurve = ncurve;
 
-rn = ones(ndomain,1);
+rn = [1,1.2];
 geom_class.rn = rn;
-lambda = 0.01;
+lambda = 2.0;
 geom_class.lambda = lambda;
 geom_class.mode = 'te';
 
@@ -80,5 +80,62 @@ potex = chnk.helm2d.kern(clmparams.k(1),srcinfo,targinfo,'s');
 
 fprintf('error in double layer test =%d\n',norm(pot-potex));
 
+%% Now test transmission problem
+% setup matrices
+fkern_s_diff = @(s,t) -chnk.helm2d.kern(clmparams.k(1),s,t,'s') + ...
+    chnk.helm2d.kern(clmparams.k(2),s,t,'s');
+fkern_dprime_diff = @(s,t) chnk.helm2d.kern(clmparams.k(1),s,t,'dprime') - ...
+    chnk.helm2d.kern(clmparams.k(2),s,t,'dprime');
 
+n = chnkrtotal.npt;
+tic, A12 = chunkermat(chnkrtotal,fkern_s_diff); toc;
+tic, A21 = chunkermat(chnkrtotal,fkern_dprime_diff); toc;
+A11 = -eye(n);
+A22 = -eye(n);
+Amat = [A11,A12;A21,A22];
 
+% setup boundary data
+rhs = zeros(2*n,1);
+
+[u1,grad1] = chnk.helm2d.green(clmparams.k(1),src(:,1),chnkrtotal.r(:,:));
+[u2,grad2] = chnk.helm2d.green(clmparams.k(2),src(:,2),chnkrtotal.r(:,:));
+
+un1 = -grad1(:,:,2);
+un2 = -grad2(:,:,2);
+
+rhs(1:n) = (u1-u2);
+rhs((n+1):end) = (un1-un2);
+
+% solve
+b = Amat\rhs;
+psi = b(1:n);
+phi = b((n+1):end);
+
+% Now test solution
+k1 = clmparams.k(1);
+k2 = clmparams.k(2);
+
+fd1  = @(s,t) chnk.helm2d.kern(k1,s,t,'d');
+fs1  = @(s,t) chnk.helm2d.kern(k1,s,t,'s');
+
+fd2  = @(s,t) chnk.helm2d.kern(k2,s,t,'d');
+fs2  = @(s,t) chnk.helm2d.kern(k2,s,t,'s');
+
+psi_res = reshape(psi,[chnkrtotal.k,chnkrtotal.nch]);
+phi_res = reshape(phi,[chnkrtotal.k,chnkrtotal.nch]);
+
+uscat_d1 = chunkerkerneval(chnkrtotal,fd1,psi_res,src(:,2));
+uscat_s1 = chunkerkerneval(chnkrtotal,fs1,phi_res,src(:,2));
+
+sol1 = uscat_d1 - uscat_s1;
+solex1 = chnk.helm2d.green(k1,src(:,1),src(:,2));
+err1 = norm(sol1-solex1);
+fprintf('error in potential in region1 = %d\n',err1);
+
+uscat_d2 = chunkerkerneval(chnkrtotal,fd2,psi_res,src(:,1));
+uscat_s2 = chunkerkerneval(chnkrtotal,fs2,phi_res,src(:,1));
+
+sol2 = uscat_d2 - uscat_s2;
+solex2 = chnk.helm2d.green(k2,src(:,1),src(:,2));
+err2 = norm(sol2-solex2);
+fprintf('error in potential in region2 = %d\n',err2);
