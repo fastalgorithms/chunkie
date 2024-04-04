1531d87d83d52c55458f2c11020f736ca1555c6b
Travis Askham <askhamwhat@gmail.com>
2024-04-04 14:55:37 -0400

    pointing whts to weights

--- chunkie/@chunker/whts.m
+++ chunkie/@chunker/whts.m
@@ -3,10 +3,6 @@ function wts = whts(chnkr)
 
 warning('whts is deprecated and will be removed, use weights instead');
 
-  k = chnkr.k;
-  nch = chnkr.nch;
-  w = chnkr.wstor;
-  wts = reshape(sqrt(sum((chnkr.d).^2,1)),k,nch);
-  wts = wts.*w(:);
+    wts = weights(chnkr);
 
 end
