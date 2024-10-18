8ea18a58fd45e5fb4ad2006d20fb10ace02ec4d0
Manas Rachh <mrachh@flatironinstitute.org>
2024-10-18 16:15:17 -0700

    curve discretizer

--- chunkie/+chnk/+smoother/smooth_curve.m
+++ chunkie/+chnk/+smoother/smooth_curve.m
@@ -1,12 +1,27 @@
-nv = 10;
-z = exp(1j*2*pi*(1:nv-1)/nv);
+nv = 3;
+z = exp(1j*2*pi*(1:nv)/nv);
 verts = [real(z); imag(z)];
 nchs = randi([1,10], 1, nv);
+k = 16;
 
-umesh = [];
-umesh.verts = verts;
-umesh.pseudo_normals = get_pseudo_normal(verts);
+kquad = 32;
 
-qmesh = [];
-qmesh
-dmesh = [];
+
+
+umesh = chnk.smoother.get_umesh(verts);
+dmesh = chnk.smoother.get_mesh(umesh, nchs, k);
+qmesh = get_mesh(umesh, nchs, kquad);
+
+figure(1)
+clf
+plot(verts(1,:), verts(2,:), 'k.','MarkerSize',20); hold on;
+plot(umesh.centroids(1,:), umesh.centroids(2,:), 'r.', 'MarkerSize',20);
+quiver(verts(1,:), verts(2,:), umesh.pseudo_normals(1,:), umesh.pseudo_normals(2,:),'k')
+quiver(umesh.centroids(1,:), umesh.centroids(2,:), umesh.face_normals(1,:), umesh.face_normals(2,:),'r')
+
+figure(2)
+clf
+plot(dmesh.r(1,:), dmesh.r(2,:),'k.'); hold on;
+axis equal;
+% quiver(dmesh.r(1,:), dmesh.r(2,:), dmesh.n(1,:), dmesh.n(2,:),'k');
+quiver(dmesh.r(1,:), dmesh.r(2,:), dmesh.pseudo_normals(1,:), dmesh.pseudo_normals(2,:),'r');
