b7e0433169474656f3027a3c114517d9839baa63
Hai Zhu <zhuh1106@gmail.com>
2024-10-18 22:18:13 -0700

    clean up stokes slp kernel split

--- chunkie/@kernel/stok2d.m
+++ chunkie/@kernel/stok2d.m
@@ -195,20 +195,24 @@ end
 
 function f = stok2d_s_split(mu,s,t)
 dist = (s.r(1,:)+1i*s.r(2,:))-(t.r(1,:)'+1i*t.r(2,:)');
+distr = real(dist);
+disti = imag(dist);
 f = cell(3, 1);
 ntarg = numel(t.r(1,:));
 nsrc = numel(s.r(1,:));
+sn1mat = repmat(s.n(1,:),[ntarg 1]);
+sn2mat = repmat(s.n(2,:),[ntarg 1]);
 f{1} = zeros(2*ntarg,2*nsrc);
 f{2} = zeros(2*ntarg,2*nsrc);
 f{3} = zeros(2*ntarg,2*nsrc);
 f{1}(1:2:end,1:2:end) = 1/(2*mu);
 f{1}(2:2:end,2:2:end) = 1/(2*mu);
-f{2}(1:2:end,1:2:end) = (-(repmat(s.n(1,:),[ntarg 1]))).*real(dist)/(2*mu);     
-f{2}(1:2:end,2:2:end) = -((repmat(s.n(2,:),[ntarg 1]))).*real(dist)/(2*mu);
-f{2}(2:2:end,1:2:end) = (-(repmat(s.n(1,:),[ntarg 1]))).*imag(dist)/(2*mu); 
-f{2}(2:2:end,2:2:end) = -((repmat(s.n(2,:),[ntarg 1]))).*imag(dist)/(2*mu);
-f{3}(1:2:end,1:2:end) = - (repmat(s.n(2,:),[ntarg 1])).*real(dist)/(2*mu);
-f{3}(1:2:end,2:2:end) =   (repmat(s.n(1,:),[ntarg 1])).*real(dist)/(2*mu);
-f{3}(2:2:end,1:2:end) = - (repmat(s.n(2,:),[ntarg 1])).*imag(dist)/(2*mu);
-f{3}(2:2:end,2:2:end) =   (repmat(s.n(1,:),[ntarg 1])).*imag(dist)/(2*mu);
+f{2}(1:2:end,1:2:end) = -sn1mat.*distr/(2*mu);     
+f{2}(1:2:end,2:2:end) = -sn2mat.*distr/(2*mu);
+f{2}(2:2:end,1:2:end) = -sn1mat.*disti/(2*mu); 
+f{2}(2:2:end,2:2:end) = -sn2mat.*disti/(2*mu);
+f{3}(1:2:end,1:2:end) = -sn2mat.*distr/(2*mu);
+f{3}(1:2:end,2:2:end) =  sn1mat.*distr/(2*mu);
+f{3}(2:2:end,1:2:end) = -sn2mat.*disti/(2*mu);
+f{3}(2:2:end,2:2:end) =  sn1mat.*disti/(2*mu);
 end
