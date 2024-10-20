ae41807f13e404433f5ea5ab14859d92733124f5
Travis Askham <askhamwhat@gmail.com>
2024-10-20 00:10:30 -0400

    Update nonflatinterface.m

--- chunkie/nonflatinterface.m
+++ chunkie/nonflatinterface.m
@@ -1,4 +1,22 @@
 function [r,d,d2] = nonflatinterface(t,a,b,c,d)
+%NONFLATINTERFACE curve parameterization of a perturbed interface given as 
+% the graph (x(t),y(t)) = (t,d e^(-at^2/2) sin(bt+c)) 
+% 
+% Syntax: 
+%    [r,d,d2] = nonflatinterface(t,a,b,c,d)
+%
+% Input: 
+%    t - array of t parameter values where curve coordinates 
+%    are desired
+%    a,b,c,d - curve parameters as in formula above 
+%
+% Output:
+%   r,d,d2 - 2 x numel(t) arrays of position, first and second 
+%   derivatives at desired t parameter values
+%
+
+% author: Solomon Quinn 
+
    xs = t;
    xp = ones(size(t));
    xpp = zeros(size(t));
