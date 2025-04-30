70a8a8f44aaac4bbdef9124a9c81de737b847f82
Tristan Goodwill <48371600+tristangdwl@users.noreply.github.com>
2025-04-30 00:13:33 -0500

    Fixed maximum last_length

--- chunkie/@chunkgraph/refine.m
+++ chunkie/@chunkgraph/refine.m
@@ -39,15 +39,12 @@ cg = balance(cg);
 
 
 if nargin == 3
-
-
-    
-
+    % if a last length is provided
     nedges = length(cg.echnks);
     nverts = length(cg.vstruc);
 
+    % first reparameterize each edge by arclength
     for i = 1:nedges
-
         len = sum(cg.echnks(i).wts(:));
         len_l = sum(cg.echnks(i).wts(:,1));
         len_r = sum(cg.echnks(i).wts(:,end));
@@ -58,37 +55,33 @@ if nargin == 3
         len_l = 2^-lvl_l*last_len;
         len_r = 2^-lvl_r*last_len;
 
-        tsplit = [len_l, 2*len_l, len - 2*len_r, len - len_r];
+        tsplit= [2*len_l, len - 2*len_r];
         tsplit = uniquetol(tsplit);
 
         mxlgth = max(sum(cg.echnks(i).wts(:,:)));
         cparams = []; cparams.maxchunklen = mxlgth;
         cparams.ta = 0; cparams.tb = len;
-        cparams.tsplits = tsplit;%cparams.eps = 1e-6;
-        % fix this to work for closed loops too
+        cparams.tsplits = tsplit;
         [~,~,info] = sortinfo(cg.echnks(i)); 
         cparams.ifclosed = info.ifclosed;
-        % cparams.ifrefine = 0; cparams.lvlr = 'n';
-
-        % cploc.nchmin = 4;
+        cparams.ifrefine = 1; cparams.lvlr = 't';
 
         param_data = chunkerarcparam_init(cg.echnks(i));
         fcurve = @(s) chunkerarcparam(s,param_data);
 
-        cparams.eps = 10 * param_data.eps;
+        cparams.eps = max(20 * param_data.eps, 1e-12);
         cg.echnks(i) = chunkerfunc(fcurve,cparams);
     end
 
-
-
-
+    % now loop over vertices ensuring agreement
     for j = 1:nverts
-
-
         loc_edges = cg.vstruc{j}{1};
         loc_dir = cg.vstruc{j}{2};
 
         nloc = length(loc_edges);
+        if nloc == 0
+            continue
+        end
         arcs = zeros(1,nloc);
 
         idch_loc = [cg.echnks(loc_edges).nch];
@@ -99,19 +92,19 @@ if nargin == 3
             arcs(k) = sum(wts(:,idch_loc(k)));
         end
 
-
+        % find desired last panel size
         lvl = get_lvl(arcs, last_len);
-
         len = last_len * 2^-lvl;
 
-        % [lvl, len, arcs]
-        % len./arcs
+        % find splitting fractions
         fracs = len./arcs;
         fracs(loc_dir==1) = 1 - fracs(loc_dir==1);
         if any(fracs<1e-8 | fracs>1-1e-8)
             if all(fracs<1e-8 | fracs>1-1e-8)
+                % nothing to do
                 continue;
             else
+                % won't be able to find splits at this lvl, so go smaller
                 lvl = lvl + 1;
                 len = last_len * 2^-lvl;
                 fracs = len./arcs;
@@ -119,19 +112,18 @@ if nargin == 3
             end
         end
 
-
         for k = 1:nloc
             split_opt = []; 
-            % split_opt.frac = len/arcs(k);
-            % if loc_dir(k) == -1
-            %     split_opt.frac = 1-split_opt.frac;
-            % end
             split_opt.frac = fracs(k);
-            % split_opt.frac
-            if split_opt.frac < 1e-8 || split_opt.frac > 1-1e-8, continue, end
-            % split_opt.frac
+            if split_opt.frac < 1e-8 || split_opt.frac > 1-1e-8
+                continue, 
+            end
+
+            % split last panel to a multiple of last_len
             cg.echnks(loc_edges(k)) = split(cg.echnks(loc_edges(k)),idch_loc(k),split_opt);
             cg.echnks(loc_edges(k)) = sort(cg.echnks(loc_edges(k)));
+
+            % now ensure that the last two panels are of correct length
             if loc_dir(k) == 1
                 cg.echnks(loc_edges(k)) = split(cg.echnks(loc_edges(k)),cg.echnks(loc_edges(k)).nch);
             else
@@ -139,22 +131,22 @@ if nargin == 3
             end
             cg.echnks(loc_edges(k)) = sort(cg.echnks(loc_edges(k)));
         end
-
-
     end
 
+    % now that vertex panels are correct, we fix the rest
     cg = refine(cg);
 end
 
 end
 
 function lvl = get_lvl(lens, last_len)
-
+    % helper routine for finding the multiple of last_len to use in length
+    % matcher
     lvl = -log2(min(lens)/last_len);
     if (lvl-round(lvl)) < 1e-8
         lvl = round(lvl);
     else
         lvl = ceil(lvl);
     end
-    lvl = max(lvl, 0);
+    lvl = max(lvl, 1);
 end
--- devtools/test/chunkgraph_lastlengthTest.m
+++ devtools/test/chunkgraph_lastlengthTest.m
@@ -1,26 +1,30 @@
-% chunkgraph_lastlengthTest0();
+chunkgraph_lastlengthTest0();
 
-% function chunkgraph_lastlengthTest0()
+function chunkgraph_lastlengthTest0()
+% test the refine option to ensure that all arclengths touching each vertex 
+% agree and are a negative power of 2 times last_len
 
-narms = 5;
-rots = exp(1i*2*pi*(1:narms)/narms);
-verts = zeros(2,2*narms);
-radi = 1;
-rado = 5;
-for i = 1:narms
-    verts(:,2*i-1) = radi*[real(rots(i));imag(rots(i))];
-    verts(:,2*i) = rado*[real(rots(i));imag(rots(i))];
+% setup vertics
+ncircedge = 6;
+rots = exp(1i*2*pi*(1:ncircedge)/ncircedge);
+verts = zeros(2,ncircedge);
+rad = 6;
+for i = 1:ncircedge
+    verts(:,i) = rad*[real(rots(i));imag(rots(i))];
 end
 nverts = size(verts,2);
+
+% setup edges
 edge2verts = [1:nverts;circshift(1:nverts,-1)];
-edge2verts = [edge2verts, [1;5]];
+edge2verts = [edge2verts, [2;6]];
+
 % curve parameters
 amp = -0.5;
-frq = 5;
+frq = 6;
 
 % define curve for each edge
 fchnks = cell(size(edge2verts,2)+1,1);
-for i = 1:narms
+for i = 1:ncircedge/2
     % odd edges are straight
     fchnks{2*i-1} = [];
     % even edges are curved
@@ -28,80 +32,67 @@ for i = 1:narms
 end
 fchnks{size(edge2verts,2)} = [];
 
-% add extra curve
-ctr = [8;0];
+% add extra closed curve
+ctr = [0;0];
 % verts = [verts, ctr];
 edge2verts = [edge2verts, [NaN; NaN]];
+narm = 3; amp = 0.3;
+fchnks{end} = @(t) starfish(t,narm,amp, ctr, 0, 0.3);
 
+% set cparams
 cparams = cell(1,size(edge2verts,2));
 for i = 1:length(cparams)
     cparams{i}.eps = 1e-8;
 end
 
-narms = 3;
-amp = 0.3;
-fchnks{end} = @(t) starfish(t,narms,amp, ctr, 0, 0.3);
 cparams{end}.ta = 0;
 cparams{end}.tb = 2*pi;
 
+% build cgrph
 cgrph = chunkgraph(verts,edge2verts,fchnks,cparams);
 
-plot(cgrph,'.')
-
+% check that we can refine a single edge
 ref_opts = cell(size(edge2verts,2),1);
-ref_opts{1}.nover = 2;
+i = 3;
+ref_opts{i}.nover = 2;
 cgrph2 = refine(cgrph,ref_opts);
 
-plot(cgrph2,'o')
+% test refine with last_len
+last_len = 2;
+cgrph3 = refine(cgrph,[],last_len);
 
-cgrph3 = cgrph;
-ichnk = 2;
-cgrph3.echnks(ichnk) = split(cgrph3.echnks(ichnk),3,struct('frac',2/12));
+ifplot = 0;
+if ifplot
+    figure(1); clf;
+    plot(cgrph3,'.'); hold on
+end
 
-clf
-plot(cgrph3,'x')
-
-
-
-clf; hold on
-last_len = 0.9;  last_len = sum(cgrph.echnks(1).wts(:,1)); last_len = 2;
-cgrph4 = refine(cgrph,[],last_len);
-% cgrph5 = refine(cgrph4,[],last_len);
-% assert(cgrph5.npt == cgrph4.npt)
-% cgrph4 = cgrph;
-plot(cgrph4,'.');axis equal
-
-hold on
-% %%
-cg = cgrph4;
-nverts = size(verts,2);
-
-
+% verify that all arclengths touching each vertex agree and are a negative
+% power of 2 times last_len
 for j = 1:nverts
-% for j = 1
-    loc_edges = cg.vstruc{j}{1};
-    loc_dir = cg.vstruc{j}{2};
+    loc_edges = cgrph3.vstruc{j}{1};
+    loc_dir = cgrph3.vstruc{j}{2};
 
     nloc = length(loc_edges);
     if nloc == 0, continue, end
     arcs = zeros(1,nloc);
 
-    idch_loc = [cg.echnks(loc_edges).nch];
+    idch_loc = [cgrph3.echnks(loc_edges).nch];
     idch_loc(loc_dir == -1) = 1;
 
     for k = 1:nloc
-        wts = cg.echnks(loc_edges(k)).wts;
+        wts = cgrph3.echnks(loc_edges(k)).wts;
         arcs(k) = sum(wts(:,idch_loc(k)));
-        plot(cg.echnks(loc_edges(k)).r(1,:,idch_loc(k)), cg.echnks(loc_edges(k)).r(2,:,idch_loc(k)),'o')
+        if ifplot
+        plot(cgrph3.echnks(loc_edges(k)).r(1,:,idch_loc(k)), cgrph3.echnks(loc_edges(k)).r(2,:,idch_loc(k)),'o')
+        end
     end
-    % [diff(arcs), log2(arcs(1) / last_len), log2(arcs(2) / last_len)-round(log2(arcs(1) / last_len))]
-    assert(log2(arcs(2) / last_len)-round(log2(arcs(1) / last_len)) < 1e-10)
+    assert(norm(diff(arcs)) < 1e-8)
+    assert(log2(arcs(1) / last_len)-round(log2(arcs(1) / last_len)) < 1e-10)
+    assert(round(log2(arcs(1) / last_len)) <= 0)
 end
 
-
-
-
-
+end
 
 
 function [r,d,d2] = sinearc(t,amp,frq)
