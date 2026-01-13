db3af5ff0321f21a046d1bf7bbe92f6fd4cfec45
Kshitij Sinha <sinhakshitij377@gmail.com>
2026-01-13 20:24:06 +0530

    Added the freq_diff to the main kernel file @kernel/kernel.m

--- chunkie/@kernel/kernel.m
+++ chunkie/@kernel/kernel.m
@@ -9,7 +9,7 @@ classdef kernel
 %      ----                                         ----
 %      'laplace'    ('lap', 'l')                    's', 'd', 'sp', 'c'
 %      'helmholtz'  ('helm', 'h')                   's', 'd', 'sp', 'dp', 'c'
-%                                                   'cp'
+%                                                   'cp', 'freq_diff'
 %      'helmholtz difference' ('helmdiff', 'hdiff') 's', 'd', 'sp', 'dp'
 %      'elasticity' ('elast', 'e')                  's', 'strac', 'd', 'dalt'
 %      'stokes'     ('stok', 's')                   'svel', 'spres', 
