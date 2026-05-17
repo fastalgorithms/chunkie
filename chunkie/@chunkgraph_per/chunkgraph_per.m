bb9efbff4d8d83460d2ca7c9e62b0e06dbde6d3a
Jonathan Shaw <91921481+Jonathan-D-Shaw@users.noreply.github.com>
2026-05-17 16:37:55 -0400

    ditching chunkgraphinregion!

--- chunkie/@chunkgraph_per/chunkgraph_per.m
+++ chunkie/@chunkgraph_per/chunkgraph_per.m
@@ -1,9 +1,32 @@
 classdef chunkgraph_per < chunkgraph
+% CHUNKGRAPH_PER is a subclass of CHUNKGRAPH that extends periodically in
+% at least one direction. 
+%
+% INPUTS: same as chunkgraph and additionally: 
+% -merge_idx: cell array with vecs, listing vertices to be merged
+%
+% properties: 
+% -vstruc_free: original unmerged vstruc from chunkgraph
+% -merge_idx: cell array, each cell containing vec of vertices to be merged
+% -vert_per: vec housing periods associated with each vertex (each belonging to
+% a periodic region). For vertices not being merged, vert_per(it) = NaN.
+%
+% methods: 
+% obj = chunkgraph_per(verts, edgesendverts, merge_idx, varargin)
+%       -initializations chunkgraph_per object
+% obj = build_vstruc(obj,merge_idx); 
+%       - computes vert_per for each vertex
+%       - loops over each cell in merge_idx, merges indices and overwrites
+%       vstruc
+%
+%OUTPUT: 
 
-    %added properties: vstruc_use, per_dir
+
+
    properties
         vstruc_free
         merge_idx
+        vert_per
    end
     methods
 
@@ -19,12 +42,17 @@ classdef chunkgraph_per < chunkgraph
             vstruc = obj.vstruc; 
             N_base_v = numel(vstruc); 
             N_merge = numel(merge_idx); 
-            N_v_use = N_base_v - N_merge; 
             idx_skip = 1:N_base_v; 
+            vper = nan(numel(obj.verts(1,:)),1); 
              for i_merge = 1:N_merge
                         Nv_merge = numel(merge_idx{i_merge});
+
                         vert_merge = []; 
                         conn_merge = []; 
+                        mi = merge_idx{i_merge}; 
+                        verts = obj.verts(:,mi);
+                        d = sqrt((verts(1,1)-verts(1,2))^2 + (verts(2,1)-verts(2,2))^2);
+                        vper(mi) = d; 
                         vert = merge_idx{i_merge}(1); 
                         edges = obj.edgesendverts; 
                         idx = edges(:) == merge_idx{i_merge};  
@@ -38,8 +66,10 @@ classdef chunkgraph_per < chunkgraph
              end
              vrem = vstruc(idx_skip); 
              v_use = [v_use, vrem]; 
-             obj.vstruc = v_use; 
-    
+
+             obj.vert_per = vper; 
+             obj.vstruc = v_use;
+   
              obj.v2emat = build_v2emat(obj); 
              obj.regions = findregions(obj); 
              obj = balance(obj); 
